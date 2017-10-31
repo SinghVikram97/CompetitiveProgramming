80e791195a7d8181feebdf5a61c115a2d7721c5f
SinghVikram97 <vikram.bedi97@gmail.com>
2017-10-31 17:03:06 +0530

    Create SquareRoot.cpp

---
+++ BinarySearch/SquareRoot.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll unsigned long long int
+ll squareRoot(ll n)
+{
+    /// largest no b/w 1 to n whose square less than n
+    ll start=1,end=n,mid;
+    ll ans;
+    while(start<=end) /// equal because for perfect square
+    {
+        mid=(start+end)/2;
+        if(mid*mid<=n)
+        {
+            ans=mid;
+            start=mid+1;
+        }
+        else
+        {
+            end=mid-1;
+        }
+    }
+    return ans;
+}
+int main()
+{
+    ll n;
+    cin>>n;
+    cout<<squareRoot(n);
+}
