925647acff7d93e572e0017d0efd92596b4f478c
SinghVikram97 <vikram.bedi97@gmail.com>
2017-10-31 17:04:12 +0530

    Update SquareRoot.cpp

--- BinarySearch/SquareRoot.cpp
+++ BinarySearch/SquareRoot.cpp
@@ -6,10 +6,10 @@ ll squareRoot(ll n)
     /// largest no b/w 1 to n whose square less than n
     ll start=1,end=n,mid;
     ll ans;
-    while(start<=end) /// equal because for perfect square
+    while(start<=end) 
     {
         mid=(start+end)/2;
-        if(mid*mid<=n)
+        if(mid*mid<=n)  /// equal because for perfect square
         {
             ans=mid;
             start=mid+1;
