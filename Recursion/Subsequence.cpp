5691604042fa6cf87701833a080ae0be43f2bab7
SinghVikram97 <vikram.bedi97@gmail.com>
2017-12-01 10:47:03 +0530

    Create Subsequence.cpp

---
+++ Recursion/Subsequence.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int length(string s)
+{
+    return pow(2,s.length());
+}
+void subsequence(string s,string out,int i)
+{
+    if(i==s.length())
+    {
+        cout<<out<<" ";
+        return;
+    }
+    /// Don't include
+    subsequence(s,out,i+1);
+    /// Include current character
+    out.push_back(s[i]);       /// Can't do out[j]=s[i]
+    subsequence(s,out,i+1);
+}
+int main()
+{
+    string s,out;
+    cin>>s;
+    cout<<length(s)<<endl;
+    subsequence(s,out,0);
+}
