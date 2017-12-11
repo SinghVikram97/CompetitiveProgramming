df6005678a0856cac4e817dfd26edd6b30a5adea
SinghVikram97 <vikram.bedi97@gmail.com>
2017-12-11 20:47:33 +0530

    Update and rename TowersOfHanoi(Count).cpp to TowersOfHanoi.cpp

--- Recursion/TowersOfHanoi.cpp
+++ Recursion/TowersOfHanoi.cpp
@@ -17,9 +17,24 @@ int steps(char source,char destination,char helper,int num_of_discs)
         return total_steps;
     }
 }
+void stepsSee(string source,string destination,string helper,int num_of_discs)
+{
+    if(num_of_discs==0 || num_of_discs==1)
+    {
+        cout<<"Move "<<1<<"th disc from "<<source<<" to "<<destination<<endl;
+        return;
+    }
+    else
+    {
+        stepsSee(source,helper,destination,num_of_discs-1);
+        cout<<"Move "<<num_of_discs<<"th disc from "<<source<<" to "<<helper<<endl;
+        stepsSee(helper,destination,source,num_of_discs-1);
+    }
+}
 int main()
 {
     int n;
     cin>>n;
+    stepsSee("T1","T2","T3",n);
     cout<<steps('A','B','C',n);
 }
