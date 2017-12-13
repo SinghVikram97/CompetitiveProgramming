7292805c0441d321e89642e206d482a1a2a76709
SinghVikram97 <vikram.bedi97@gmail.com>
2017-12-13 09:04:40 +0530

    Update Ways(OrderDoesn'tMatter).cpp

--- Dynamic programming/CoinChange/Ways(OrderDoesn'tMatter).cpp
+++ Dynamic programming/CoinChange/Ways(OrderDoesn'tMatter).cpp
@@ -3,25 +3,23 @@
 using namespace std;
 int number_of_ways(int total_value,vector<int> coins,int index)
 {
-    if(total_value>0 && index==coins.size()) /// Run out of coins but not money
-    {
-        return 0;
-    }
-    else if(total_value==0)
-    {
-        return 1;
-    }
-    else
-    {
-        int ways=0;
-        while(total_value>=0)
-        {
-            /// 0 coins of current coin
-           ways=ways+number_of_ways(total_value,coins,index+1);
-           total_value=total_value-coins[index];
-        }
-        return ways;
-    }
+   if(total_value<0)  /// Imp condition
+   {
+       return 0;
+   }
+   else if(total_value==0)
+   {
+     return 1; /// Sucessful
+   }
+   else if(total_value>0 && index==coins.size())  /// Run out of coins but not money
+   {
+     return 0; /// Unsucessful
+   }
+   else
+   {
+     /// Le skte ho ya nhi le skte
+     return number_of_ways(total_value-coins[index],coins,index)+number_of_ways(total_value,coins,index+1);
+   }
 }
 int main()
 {
@@ -34,3 +32,4 @@ int main()
     }
     cout<<number_of_ways(n,v,0);
 }
+
