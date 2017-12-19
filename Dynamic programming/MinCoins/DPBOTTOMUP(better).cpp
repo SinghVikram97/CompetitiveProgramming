7a898f54b29c504c6dc9c1389e36b06c9a25c4ab
SinghVikram97 <vikram.bedi97@gmail.com>
2017-12-19 13:11:43 +0530

    Update DPBOTTOMUP(better).cpp

--- Dynamic programming/MinCoins/DPBOTTOMUP(better).cpp
+++ Dynamic programming/MinCoins/DPBOTTOMUP(better).cpp
@@ -3,33 +3,42 @@
 ///  15
 #include<bits/stdc++.h>
 using namespace std;
-#define max 1000000
+#define max 100000
 /// Can't use INT_MAX as 1+INT_MAX returns negative value
 int min_coins(vector<int> coins,int amount)
 {
    int dp[amount+1]; /// dp[i] is min_coins for amount i
    dp[0]=0; /// 0 ways to give 0 ruppees
-   int ways=max;
+   for(int i=1;i<=amount;i++)
+   {
+       dp[i]=max;  /// Assume sab mein abhi calculate nhi hua
+   }
    int current_ways;
    for(int i=1;i<=amount;i++)
    {
       /// Koi bhi ek coin pick kar skte hao
-      for(int i=0;i<coins.size();i++)
+      for(int j=0;j<coins.size();j++)
       {
           /// Can pick a coin if and only if amount-coins[i]>=0
           /// else dp[amount-coins[i]] out of index error
-          if(amount-coins[i]>=0)
+          if(i-coins[j]>=0)
           {
-            current_ways=1+dp[amount-coins[i]];
-            if(current_ways<ways)
+            current_ways=1+dp[i-coins[j]];
+            if(current_ways<dp[i]) /// Agar is coin ko lene se kam coins lag rhe hai overall toh update value
             {
-              dp[amount]=current_ways;
+              dp[i]=current_ways;
             }
           }
       }
    }
-   return dp[amount];
-
+   if(dp[amount]==max)
+   {
+       return -1;
+   }
+   else
+   {
+       return dp[amount];
+   }
 }
 int main()
 {
