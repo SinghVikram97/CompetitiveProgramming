ae1700c54529a4be34732baeac375e17f7a841ec
SinghVikram97 <vikram.bedi97@gmail.com>
2018-01-20 19:54:21 +0530

    Update A_(24) PrintStronglyConnectedComponents.cpp

--- Graphs/Basic/A_(24) PrintStronglyConnectedComponents.cpp
+++ Graphs/Basic/A_(24) PrintStronglyConnectedComponents.cpp
@@ -1,3 +1,9 @@
+https://drive.google.com/open?id=15FsrMEpaaVFU96YM1wvQbosqki7dcYA6
+https://drive.google.com/open?id=1_sf9PM90ws9XIuz5l2FLunaN2kUAbd7R
+https://drive.google.com/open?id=19xZZRNlEbsnWhspsKSCs8PnDO2hf45n0
+1) Create an empty stack ‘S’ and do DFS traversal of a graph. In DFS traversal, after calling recursive DFS for adjacent vertices of a vertex, push the vertex to stack. In the above graph, if we start DFS from vertex 0, we get vertices in stack as 1, 2, 4, 3, 0.
+2) Reverse directions of all arcs to obtain the transpose graph.
+3) One by one pop a vertex from S while S is not empty. Let the popped vertex be ‘v’. Take v as source and do DFS (call DFSUtil(v)). The DFS starting from v prints strongly connected component of v. In the above example, we process vertices in order 0, 3, 4, 2, 1 (One by one popped from stack).
 #include<bits/stdc++.h>
 using namespace std;
 template<typename T>
