26c98a2b35faea21afb39decd65f9cc0c0fd5cde
SinghVikram97 <vikram.bedi97@gmail.com>
2018-01-29 22:48:54 +0530

    Create A_(4) Square root (Floor) Big Numbers.cpp

---
+++ BinarySearch/A_(4) Square root (Floor) Big Numbers.cpp
@@ -0,0 +1,34 @@
+/// TRY FOR 1000000000000000000
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+ll square_root(ll n)
+{
+    if(n==0)
+        return n;
+    ll s,e,mid;
+    s=1,e=n;
+    ll ans;
+    while(s<=e)
+    {
+        mid=(s+e)/2;
+        if(mid==n/mid)
+            return mid;
+        else if(mid<n/mid)
+        {
+            ans=mid;
+            s=mid+1;
+        }
+        else
+        {
+            e=mid-1;
+        }
+    }
+    return ans;
+}
+int main()
+{
+    ll n;
+    cin>>n;
+    cout<<square_root(n);
+}
