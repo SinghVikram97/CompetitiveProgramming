b95c1b48bb12c321009c6539b96bf5143412eefb
SinghVikram97 <vikram.bedi97@gmail.com>
2018-01-29 17:38:11 +0530

    Update A_(0) Questions.cpp

--- Segment-Trees/A_(0) Questions.cpp
+++ Segment-Trees/A_(0) Questions.cpp
@@ -1,3 +1,4 @@
 https://hack.codingblocks.com/practice/p/33/309
 Passing by reference solves TLE problem.
 https://www.hackerearth.com/problem/algorithm/range-update-range-max-queries/description/
+Resolving a node 1st without checking for Overlaps solve wrong answer
