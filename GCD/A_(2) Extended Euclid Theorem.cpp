ceafe92b45ca0ac0dc29b3df919e7dc57574f133
SinghVikram97 <vikram.bedi97@gmail.com>
2018-02-05 12:27:22 +0530

    Create A_(2) Extended Euclid Theorem.cpp

---
+++ GCD/A_(2) Extended Euclid Theorem.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+pair<int,int> gcd(int a,int b)
+{
+    if(b==0)
+    {
+        return make_pair(1,0);
+    }
+    int currentX,currentY;
+    pair<int,int> nextXY=gcd(b,a%b);
+    currentX=nextXY.second;
+    currentY=nextXY.first-(floor(a/b)*nextXY.second);
+    return make_pair(currentX,currentY);
+}
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    pair<int,int> ans=gcd(a,b);
+    cout<<ans.first<<" "<<ans.second<<endl;
+}
