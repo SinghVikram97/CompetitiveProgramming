6822e12b00d56c926d46a9e30509307b08436c41
SinghVikram97 <vikram.bedi97@gmail.com>
2018-02-07 08:38:07 +0530

    Create A_(2) 2's Complement and Even or Odd.cpp

---
+++ Bitmasking/A_(2) 2's Complement and Even or Odd.cpp
@@ -0,0 +1,18 @@
+https://drive.google.com/open?id=1vix6Ixc1kFbczwynSmFIJyDTbb1unN3y
+https://drive.google.com/open?id=1q86g1_vLynLySvFxls0lqlutHI5P3LNo
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n&1)
+    {
+        cout<<"Odd"<<endl;
+    }
+    else
+    {
+        cout<<"Even"<<endl;
+    }
+}
